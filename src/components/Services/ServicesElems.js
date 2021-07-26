55e84e82b52cefdc4b65c173838540a701a0aeb6
Zobee <zohebboga@hotmail.com>
2021-07-25 20:41:56 -0400

    Fixed minor css bug

--- src/components/Services/ServicesElems.js
+++ src/components/Services/ServicesElems.js
@@ -18,6 +18,13 @@ export const ServicesContainer = styled.div`
 `
 
 export const ServicesH1 = styled.h1`
+  font-size: 2.5rem;
+  color: #fff;
+  margin-bottom: 64px;
+
+  @media screen and (max-width: 480px) {
+    font-size: 2rem;
+  }
 `
 
 export const ServicesWrapper = styled.div`
@@ -42,7 +49,8 @@ export const ServicesWrapper = styled.div`
 export const ServicesCard = styled.div`
   background: #FFF;
   display: flex;
-  flex-direction: flex-start;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
   border-radius: 10px;
   max-height: 340px;
@@ -55,11 +63,18 @@ export const ServicesCard = styled.div`
   }
 `
 
-export const ServicesIcon = styled.div`
+export const ServicesIcon = styled.img`
+  height: 160px;
+  width: 160px;
+  margin-bottom: 10px;
 `
 
 export const ServicesH2 = styled.h2`
+  font-size: 1rem;
+  margin-bottom: 10px;
 `
 
 export const ServicesP = styled.p`
+  font-size: 1rem;
+  text-align: center;
 `
