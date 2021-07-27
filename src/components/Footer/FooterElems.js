379caac4e5b74ad80e5014d79b3b5038035e1b82
Zobee <zohebboga@hotmail.com>
2021-07-26 20:17:54 -0400

    Added basic footer social media section

--- src/components/Footer/FooterElems.js
+++ src/components/Footer/FooterElems.js
@@ -66,20 +66,53 @@ export const FooterLink = styled(Link)`
     color: #01BF71;
   }
 `
-export const SocialMedia = styled.div`
+export const SocialMedia = styled.section`
+  max-width: 1000px;
+  width: 100%;
 `
 
 export const SocialMediaWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1100px;
+  margin: 40px auto 0 auto;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
 `
 
-export const SocialLogo = styled.div`
+export const SocialLogo = styled(Link)`
+  color: #FFF;
+  justify-self: start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+  font-weight: bold;
 `
 
-export const WebRights = styled.div`
+export const WebRights = styled.small`
+  color: #FFF;
+  margin-bottom: 16px;
 `
 
 export const SocialIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 240px;
 `
 
-export const SocialIconLink = styled.div`
+export const SocialIconLink = styled.a`
+  color: #FFF;
+  font-size: 24px;
+  transition: all 0.25s ease;
+
+  &:hover {
+    color: #01BF71;
+  }
 `
